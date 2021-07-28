33cc70f61d2a075ff2aa99cd76d9769a68c97155
Skyline-23 <bs2740@naver.com>
2021-07-28 17:48:52 +0900

    [ADD] BaseViewController

---
+++ rxflow-test/Source/Base/BaseViewController.swift
@@ -0,0 +1,72 @@
+//
+//  BaseViewController.swift
+//  rxflow-test
+//
+//  Created by 김부성 on 2021/07/28.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+import Then
+import SnapKit
+
+class BaseViewController: UIViewController {
+    
+    // MARK: - UI
+    let activityIndicatorView = UIActivityIndicatorView(style: .large).then {
+        $0.color = .secondarySystemBackground
+    }
+    
+    // MARK: - Initializing
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        self.activityIndicatorView.stopAnimating()
+    }
+    
+    // MARK: - Rx
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupLayout()
+        self.makeConstraints()
+        self.setupLocalization()
+
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(self.activityIndicatorView)
+        
+        activityIndicatorView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Methods
+    
+    func setupLayout() {
+        // add Views
+    }
+    
+    func makeConstraints() {
+        // Constraints
+    }
+    
+    func setupLocalization() {
+        // localizations
+    }
+    
+    
+
+}
