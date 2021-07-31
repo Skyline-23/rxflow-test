9f0fc42938bfdced391d31b77422e289e3a71d75
Skyline-23 <bs2740@naver.com>
2021-07-31 22:20:57 +0900

    [ADD] HomeFlow

---
+++ rxflow-test/Source/Flow/HomeFlow.swift
@@ -0,0 +1,55 @@
+//
+//  HomeFlow.swift
+//  rxflow-test
+//
+//  Created by 김부성 on 2021/07/31.
+//
+
+import RxFlow
+
+class HomeFlow: Flow {
+    
+    // MARK: - Properties
+    var root: Presentable {
+        return self.rootViewController
+    }
+    
+    private lazy var rootViewController = UINavigationController()
+    
+    // MARK: - Initalizing
+    init() {
+        
+    }
+    
+    deinit {
+        print("❎ \(type(of: self)): \(#function)")
+    }
+    
+    // MARK: - Switch Navigation
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? DemoStep else { return .none }
+        
+        switch step {
+        
+        case .homeIsRequired:
+            return self.navigateToHome()
+            
+        case .loginIsRequired:
+            return .end(forwardToParentFlowWithStep: DemoStep.loginIsRequired)
+            
+        default:
+            return .none
+        }
+    }
+}
+
+extension HomeFlow {
+    
+    private func navigateToHome() -> FlowContributors {
+        let reactor = HomeViewReactor()
+        let viewController = HomeViewController(reactor: reactor)
+        
+        self.rootViewController.setViewControllers([viewController], animated: false)
+        return .one(flowContributor: .contribute(withNextPresentable: viewController, withNextStepper: reactor))
+    }
+}
